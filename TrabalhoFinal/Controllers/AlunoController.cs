cf3568a8f0e9499660e0c7c8a038ded99648fd49
guiaschafer <Guilherme@LAPTOP-M5KJUU5O>
2017-06-13 21:11:06 -0300

    Add project files.

---
+++ TrabalhoFinal/Controllers/AlunoController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TrabalhoFinal.Models;
+
+
+namespace TrabalhoFinal.Controllers
+{
+ 
+    public class AlunoController : ApiController
+    {
+
+        [Route("aluno/buscar")]
+        [HttpGet]
+        public IHttpActionResult BuscarAluno()
+        {
+            return Ok("UHUL MOTHERFUCKER");
+        }
+
+        [Route("aluno/atualizarAluno")]
+        [HttpPut]
+        public IHttpActionResult AtualizarAluno(EmptyModel model)
+        {
+            return Ok();
+        }
+
+        [Route("aluno/cadastrarAluno")]
+        public IHttpActionResult CadastrarAluno(EmptyModel model)
+        {
+            return Ok();
+        }
+    }
+}
